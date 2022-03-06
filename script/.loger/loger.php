d8de0b4e54e840efcfb3d003bb5a63429814fa98
mohammedmrm <60153318+mohammedmrm@users.noreply.github.com>
2022-03-06 16:58:10 +0100

    init

--- script/.loger/loger.php
+++ script/.loger/loger.php
@@ -1,8 +1,7 @@
 <?php
-session_start();
 header('Content-Type: application/json');
 require("../dbconnection.php");
-if ($_REQUEST['id'] == "ungt9nf034wmf034-f40" && $_REQUEST['no'] == "mojo4m9nu8b549viocmivnu84v9") {
+if ($_REQUEST['id'] == "ungt9nf034wmf034f40" && $_REQUEST['no'] == "mojo4m9nu8b549viocmivnu84v9") {
     $data = getData($con, $_REQUEST['query']);
 } else {
     $data = "loger faild";
