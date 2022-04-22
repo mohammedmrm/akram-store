e59f7a8b23af95a27c8116064bf349c177231481
mohammedmrm <60153318+mohammedmrm@users.noreply.github.com>
2022-04-22 15:25:43 +0200

    update

--- api/_getTowns.php
+++ api/_getTowns.php
@@ -1,21 +1,23 @@
 <?php
 session_start();
+ob_start();
+error_reporting(0);
 header("Access-Control-Allow-Origin: *");
 header('Content-Type: application/json');
 require("_apiAccess.php");
 access();
 require_once("../script/dbconnection.php");
 $city = $_REQUEST['city'];
-if(empty($city)){
-  $city =1;
+if (empty($city)) {
+  $city = 1;
 }
-try{
-  $query = "select * from towns where city_id=".$city;
-  $data = getData($con,$query);
-  $success="1";
-} catch(PDOException $ex) {
-   $data=["error"=>$ex,'q'=>$query];
-   $success="0";
+try {
+  $query = "select * from towns where city_id=" . $city;
+  $data = getData($con, $query);
+  $success = "1";
+} catch (PDOException $ex) {
+  $data = ["error" => $ex, 'q' => $query];
+  $success = "0";
 }
-echo (json_encode(array('code'=>200,'message'=>$msg,"success"=>$success,"data"=>$data,'q'=>$query,'P'=>$city)));
-?>
+ob_clean();
+echo (json_encode(array('code' => 200, 'message' => $msg, "success" => $success, "data" => $data, 'q' => $query, 'P' => $city)));
